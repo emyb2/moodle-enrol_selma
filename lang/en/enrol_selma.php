fae627affc201f90f08259c32b3e355df9c93c38
Troy Williams <maxorsomeness@gmail.com>
2020-09-04 19:52:39 +1200

    Remove backticks from language strings.

--- lang/en/enrol_selma.php
+++ lang/en/enrol_selma.php
@@ -288,11 +288,12 @@ $string['updatefunctions'] = '    Updating the SELMA webservices functions witho
 $string['updatefunctionsdone'] = '    The new webservice functions should be visible.';
 $string['executiontime'] = '  Script execution took {$a} seconds.';
 
-$string['characterlengthexceeded'] = 'Character length exceeded expected `{$a-expected}` recieved `{$a->recieved}`';
+$string['characterlengthexceeded'] = 'Character length exceeded expected \'{$a-expected}\' received \'{$a->recieved}\'';
 $string['maximumcharacterlengthexceeded'] = 'Maximum character length exceeded';
-$string['unexpectedvalue'] = 'Unexpected value for `{$a}`';
-$string['invalidargument'] = 'Invalid argument for `{$a}`';
-$string['invalidargumentexpects'] = 'Invalid argument for `{$a->name}` expects type `{$a->type}`';
+$string['maximumcharacterlengthforexceeded'] = 'Maximum character length for \'{$a}\' exceeded';
+$string['unexpectedvalue'] = 'Unexpected value for \'{$a}\'';
+$string['invalidargument'] = 'Invalid argument for \'{$a}\'';
+$string['invalidargumentexpects'] = 'Invalid argument for \'{$a->name}\' expects type \'{$a->type}\'';
 
 $string['servicerolename'] = 'Web service';
 $string['serviceroledescription'] = 'Used by external systems to communicate with Moodle.';
