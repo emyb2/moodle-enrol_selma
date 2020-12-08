9ed762506c276c5775f310d8b1759bc9407cc387
zpottie <zander.potgieter@learningworks.co.nz>
2020-12-08 16:15:20 +1300

    Bump version number

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'enrol_selma';        // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
 $plugin->release    = '0.5.2 (MOOMA)';      // Human-readable release version.
-$plugin->version    = 2020112320;           // YYYYMMDDHH (year, month, day, 24-hr format hour).
+$plugin->version    = 2020120816;           // YYYYMMDDHH (year, month, day, 24-hr format hour).
 $plugin->requires   = 2018051703;           // YYYYMMDDHH (Version number for Moodle 3.5.3).
 $plugin->maturity   = MATURITY_ALPHA;       // Code maturity/stability.
